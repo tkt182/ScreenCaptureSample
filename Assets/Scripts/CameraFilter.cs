78b99b557b63a68e44136464ebc295ebd0ce1913
shinichiro takata <shinichiro.takata@gmail.com>
2020-05-02 13:12:24 +0900

    Initial Commit

---
+++ Assets/Scripts/CameraFilter.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class CameraFilter : MonoBehaviour
+{
+    [SerializeField]
+    private Material filter;
+
+    //nanoKontrol2を宣言
+    private NanoKontrol2 nanoKontrol2;
+    private int property_width_ID;
+    private int property_height_ID;
+
+    void Start()
+    {
+        //Start()の中でコールバック関数の設定を行う
+        nanoKontrol2 = GameObject.Find("NanoKontrol2").GetComponent<NanoKontrol2>();
+        nanoKontrol2.valueChangedFunctions.Add(nanoKontrol2_valueChanged);
+        nanoKontrol2.keyPushedFunctions.Add(nanoKontrol2_keyPushed);
+        property_width_ID = Shader.PropertyToID("_Width");
+        property_height_ID = Shader.PropertyToID("_Height");
+    }
+
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        
+        Graphics.Blit(src, dest, filter);
+    }
+
+    //キー（スライダー・ノブ）の値が変更された場合に呼び出される関数
+    public void nanoKontrol2_valueChanged(string keyName, int keyValue)
+    {
+        Debug.Log(keyName);
+        Debug.Log(keyValue);
+        filter.SetInt(property_width_ID, 1920 * keyValue / 255);
+        filter.SetInt(property_height_ID, 1080 * keyValue / 255);
+    }
+
+    //キーが押された場合に呼び出される関数
+    public void nanoKontrol2_keyPushed(string keyName)
+    {
+        Debug.Log(keyName);
+    }
+
+}
---
+++ Assets/Scripts/NanoKontrol.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NanoKontrol : MonoBehaviour
+{
+
+    //nanoKontrol2を宣言
+    private NanoKontrol2 nanoKontrol2;
+
+    void Start()
+    {
+        //Start()の中でコールバック関数の設定を行う
+        nanoKontrol2 = GameObject.Find("NanoKontrol2").GetComponent<NanoKontrol2>();
+        nanoKontrol2.valueChangedFunctions.Add(nanoKontrol2_valueChanged);
+        nanoKontrol2.keyPushedFunctions.Add(nanoKontrol2_keyPushed);
+    }
+
+
+    //キー（スライダー・ノブ）の値が変更された場合に呼び出される関数
+    public void nanoKontrol2_valueChanged(string keyName, int keyValue)
+    {
+        //Debug.Log(keyName);
+        //Debug.Log(keyValue);
+
+        int propertyID = Shader.PropertyToID("_Width");
+        //GetComponent<Renderer>().material.SetInt(propertyID, 1920 / keyValue);
+        Debug.Log(propertyID);
+        GetComponent<Renderer>().material.SetInt(propertyID, 1920 / keyValue);
+    }
+
+    //キーが押された場合に呼び出される関数
+    public void nanoKontrol2_keyPushed(string keyName)
+    {
+        Debug.Log(keyName);
+    }
+
+
+    //任意のタイミングでスライダーやノブの値の取得
+    void Update()
+    {
+        //Debug.Log (nanoKontrol2.Slider1);
+        //Debug.Log (nanoKontrol2.Slider2);
+        //Debug.Log (nanoKontrol2.knob1);
+        //Debug.Log (nanoKontrol2.knob2);
+    }
+
+}
